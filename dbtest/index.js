7ff23f6c2585c8782c96307b36a8176cd51a8ab3
Russell <email.rpau@gmail.com>
2024-10-27 08:52:15 -0400

    small update

--- dbtest/index.js
+++ dbtest/index.js
@@ -3,7 +3,6 @@ import { logCall } from './model/Call.js';
 import { returnCall } from './model/Call.js';
 import 'dotenv/config';
 
-const srv = process.env.MONGO_SRV
 
 mongoose.connect(`${process.env.MONGO_SRV}`)
 let check = logCall(2, 18002672001);
@@ -11,5 +10,5 @@ let check = logCall(2, 18002672001);
 
 const callList = await returnCall(2);
 
-console.log(callList[1]);
-console.log(callList[1].sus);
+console.log(callList[0]);
+console.log(callList[0].sus);
